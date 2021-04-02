848748176b3b37b7f88c9ebff5da557bc91bc617
Jens Streck <jens.streck@gmail.com>
2021-04-01 22:29:06 -0500

    setup router

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
 mod socket;
 
-use std::convert::Infallible;
-use std::net::SocketAddr;
 use hyper::service::{make_service_fn, service_fn};
-use hyper::{Body, Request, Response, Server};
+use hyper::{Body, Request, Response, Server, Method, StatusCode};
 // use std::{fs, thread};
 
 // async fn image(req: HttpRequest) -> HttpResponse {
@@ -14,30 +12,30 @@ use hyper::{Body, Request, Response, Server};
 //         .body(data);
 // }
 
-async fn hello_world(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
-    Ok(Response::new("Hello, World".into()))
+async fn route_handler(req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
+    match(req.method(), req.uri().path()) {
+        (&Method::GET, "/") => Ok(Response::new(Body::from("Hello World"))),
+        
+        // Return the 404 Not Found for other routes.
+        _ => {
+            let mut not_found = Response::default();
+            *not_found.status_mut() = StatusCode::NOT_FOUND;
+            Ok(not_found)
+        }
+    }
 }
 
 #[tokio::main]
-async fn main() {
-    // thread::spawn(move || {
-    //     // connection succeeded
-    //     socket::start();
-    // });
-    // We'll bind to 127.0.0.1:8080
-    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let addr = ([127, 0, 0, 1], 8080).into();
 
-    // A `Service` is needed for every connection, so this
-    // creates one from our `hello_world` function.
-    let make_svc = make_service_fn(|_conn| async {
-        // service_fn converts our function into a `Service`
-        Ok::<_, Infallible>(service_fn(hello_world))
-    });
+    let service = make_service_fn(|_| async { Ok::<_, hyper::Error>(service_fn(route_handler)) });
 
-    let server = Server::bind(&addr).serve(make_svc);
+    let server = Server::bind(&addr).serve(service);
 
-    // Run this server for... forever!
-    if let Err(e) = server.await {
-        eprintln!("server error: {}", e);
-    }
+    println!("Listening on http://{}", addr);
+
+    server.await?;
+
+    Ok(())
 }
